5be256fa9fea4c2a30c419b92f79065f77adcb6b
Shoma Takami <hawklook.2331@gmail.com>
2019-09-12 16:53:35 -0700

    popup2

--- src/component/popup.js
+++ src/component/popup.js
@@ -1,23 +1,27 @@
 import React from "react";
 
 import "./popup.css";
+import "../index.css";
 
 function Popup() {
   return (
     <>
-      <div class='popup' id='js-popup'>
-        <div class='popup-inner'>
-          <div class='close-btn' id='js-close-btn'>
-            <i class='fas fa-times'></i>
-          </div>
-          <a href='#'>
-            <img src='../popup.jpg' alt='popup-pic' />
+      <div className='content'>
+        <a className='js-modal-open' href=''>
+          <i className='fas fa-pen'></i>
+        </a>
+      </div>
+      <div className='modal js-modal'>
+        <div className='modal__bg js-modal-close'></div>
+        <div className='modal__content'>
+          <p>
+            ここにモーダルウィンドウで表示したいコンテンツを入れます。モーダルウィンドウを閉じる場合は下の「閉じる」をクリックするか、背景の黒い部分をクリックしても閉じることができます。
+          </p>
+          <a className='js-modal-close' href=''>
+            閉じる
           </a>
         </div>
-        <div class='black-background' id='js-black-bg'></div>
       </div>
-      <button id='js-show-popup'>Show Popup</button>
-      {/* <i className='fas fa-pen'></i> */}
     </>
   );
 }
